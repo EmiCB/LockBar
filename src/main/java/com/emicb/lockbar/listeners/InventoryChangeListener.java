2c60f295aeef55247d95754df9b26b78388703ff
Emi Brown <emicb55@gmail.com>
2022-06-27 10:17:42 -0500

    Fixed /lock-bar Locking Entire Inventory

--- src/main/java/com/emicb/lockbar/listeners/InventoryChangeListener.java
+++ src/main/java/com/emicb/lockbar/listeners/InventoryChangeListener.java
@@ -3,7 +3,7 @@ package com.emicb.lockbar.listeners;
 import com.emicb.lockbar.Lockbar;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -26,17 +26,22 @@ public class InventoryChangeListener implements Listener {
         // exit if inventory not clicked
         if (event.getClickedInventory() == null) return;
 
+        // get player
+        HumanEntity player = event.getWhoClicked();
+
         // check if global inventory lock is enabled
         if (config.getBoolean("lock-all")) {
+            player.setItemOnCursor(new ItemStack(Material.AIR));
             event.setCancelled(true);
         }
 
         // check if global hotbar lock is enabled
         if (config.getBoolean("lock-bar")) {
-            if (event.getView().getBottomInventory().getType().equals(InventoryType.PLAYER)) {
-                event.getWhoClicked().setItemOnCursor(new ItemStack(Material.AIR));
+            if (event.getSlotType().equals(InventoryType.SlotType.QUICKBAR)) {
+                player.setItemOnCursor(new ItemStack(Material.AIR));
                 event.setCancelled(true);
             }
+
         }
     }
 }
