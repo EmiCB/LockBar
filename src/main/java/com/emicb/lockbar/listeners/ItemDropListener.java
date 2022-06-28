0296f7038857161665bf837631351adf2e2b97c6
Emi Brown <emicb55@gmail.com>
2022-06-28 12:18:34 -0500

    Fixed Item Drops From Inventory

--- src/main/java/com/emicb/lockbar/listeners/ItemDropListener.java
+++ src/main/java/com/emicb/lockbar/listeners/ItemDropListener.java
@@ -1,11 +1,13 @@
 package com.emicb.lockbar.listeners;
 
 import com.emicb.lockbar.Lockbar;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Listens for events where items could be dropped.
@@ -28,13 +30,14 @@ public class ItemDropListener implements Listener {
 
         // check if global hotbar lock is enabled
         if (config.getBoolean("lock-bar")) {
-            // exit if player has inventory open (InventoryType set to CRAFTING if no inventory open)
-            if (event.getPlayer().getOpenInventory().getType() != InventoryType.CRAFTING) return;
-
-            // disable dropping items from hotbar slots
-            int slot = event.getPlayer().getInventory().getHeldItemSlot();
-            if (0 <= slot && slot <= 8) {
-                event.setCancelled(true);
+            // check material types to determine if item is in hotbar
+            Material drop_type = event.getItemDrop().getItemStack().getType();
+            for (int i = 0; i < 8; i++) {
+                ItemStack slot = event.getPlayer().getInventory().getItem(i);
+                if (slot != null && drop_type.equals(slot.getType())) {
+                    Bukkit.getConsoleSender().sendMessage("Items match!");
+                    event.setCancelled(true);
+                }
             }
 
         }
